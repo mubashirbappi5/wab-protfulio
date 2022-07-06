b219578735f79427631dfe74a0ef610d40f95fbb
mubashirusha <mubashirbappi@gmail.com>
2022-07-06 23:50:13 +0600

    first work done

---
+++ new 2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+int main()
+
+{
+       int e=76;
+	    
+	   
+	   int f=78;
+	   
+	   
+	   int sum=e+f;
+	   
+	   
+	   printf("%d+%d= %d,e,f,sum);
+	   
+	   
+	   
+return 0;
+
+}
